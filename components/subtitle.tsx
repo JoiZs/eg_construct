f2166e83515ca081fc2a7299088e6965305f2eb4
SeeeSaww <pyaesoneaung1126@hotmail.com>
2021-12-11 11:15:14 +0630

    modifysubti

--- components/subtitle.tsx
+++ components/subtitle.tsx
@@ -1,28 +1,30 @@
 import React, { useContext, useState } from 'react'
 import ChangeHistoryIcon from '@mui/icons-material/ChangeHistory';
 import { SectionContext } from './sectConte';
+import gsap from 'gsap/all';
 
 interface Props {
     
 }
 export const Subtitle = (props: Props) => {
     const [section, setSection] = useContext(SectionContext)
-    let sectTop = 0
-
     if(section[0] ==1 ){
-        sectTop = 0;
+        gsap.to('#subti',{duration:0.2, y:'0rem', ease:'power2.out'})
     }else if(section[1] ==1 ){
-        sectTop = 8;
+        gsap.to('#subti',{duration:0.2, y:'-2rem', ease:'power2.out'})
+
     }else if(section[2] ==1 ){
-        sectTop = 16;
+        gsap.to('#subti',{duration:0.2, y:'-4rem', ease:'power2.out'})
+
     }else if(section[3] ==1 ){
-        sectTop = 24;
+        gsap.to('#subti',{duration:0.2, y:'-6rem', ease:'power2.out'})
+
     }
     
     return (
-        <div className='fixed w-full text-xs px-2 h-8 list-none leading-8 overflow-hidden z-10'>
+        <div className='fixed w-full text-xs px-2 h-8 list-none leading-8 overflow-hidden z-10 top-2'>
             <ChangeHistoryIcon fontSize='small' className='transform rotate-90' />
-            <h1 className={`italic pl-6 uppercase tracking-widest absolute transition-all -top-${sectTop}`}>
+            <h1 id='subti' className={`italic pl-6 uppercase tracking-widest absolute transition-all top-0`}>
                 <li>Home</li>
                 <li>Services</li>
                 <li>About</li>
