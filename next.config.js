0e04434c92b87b3de219b60af4b1fc3ea70fbc92
SeeeSaww <pyaesoneaung1126@hotmail.com>
2021-12-12 09:54:28 +0630

    nextconfig modify

--- next.config.js
+++ next.config.js
@@ -1,20 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: false,
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.mp4$/,
-      use: [
-        {
-          loader: "file-loader",
-          options: {
-            name: "[name].[ext]",
-            outputPath: "video",
-          },
-        },
-      ],
-    });
-
-    return config;
-  },
 };
